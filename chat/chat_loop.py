91a3ad65917e443fbdfb388342a17f2e6974b3e7
horseshoeman-clou <218686597+horseshoeman-clou@users.noreply.github.com>
2026-05-30 14:42:57 +0530

    Add command routing and help system

--- chat/chat_loop.py
+++ chat/chat_loop.py
@@ -1,13 +1,36 @@
 from model.dummy_model import get_response
 
+def handle_command(input):
+	if input=="/help":
+		return"""
+	Available commands:
+	/help - Show available commands
+	/exit - Exit NanoChat
+	"""
+	elif input=="/exit":
+		return "EXIT_COMMAND"
+
+	else:
+		return f"""
+	No command called {input}.
+	Type /help to see available commands.
+	"""
+
 def start_chat():
-	print("NanoChat started. Type 'exit' to quit.\n")
+	print("""
+	Welcome to NanoChat!
+	Type /help for available commands.\n""")
+
 	while True:
 		user_input=input("You: ")
 
-		if user_input.lower()=='exit':
-			print("Exiting nanoChat.")
-			break
+		if user_input.lower().startswith('/'):
+			response=handle_command(user_input.lower())
+
+			if response=="EXIT_COMMAND":
+				break
+			print(response)
+			continue
 
 		response=get_response(user_input)
 		print(f"Bot: {response}")
